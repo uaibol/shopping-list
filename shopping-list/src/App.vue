de9f688563e4c7c4533af6683dfa6f952291ef46
uaibol <uaibol@gmail.com>
2023-06-14 17:36:01 +0600

    app

---
+++ shopping-list/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="myclass">{{ header }}</h1>
+  <ul>
+    <li v-for="{id, label} in items" :key="id">{{ label }}</li>
+  </ul>
+
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const header = ref("Shopping List App")
+const items = ref([
+  {id: 1, label: "10 party hats"},
+  {id: 2, label: "2 board games"},
+  {id: 3, label: "20 caps"}
+])
+
+
+</script>
+
